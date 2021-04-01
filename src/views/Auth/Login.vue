b679506c3b0e690acc534e2441a224b2f225718d
Daniel Apodaca <apodacaduron@gmail.com>
2021-04-01 07:47:18 -0700

    feat(validations): added vuelidate on login and signup forms

--- src/views/Auth/Login.vue
+++ src/views/Auth/Login.vue
@@ -3,25 +3,57 @@
   Nav(onlyLogo, :spaceBetween="false")
   .sized-box.df.df-center
     .form-container.df.df-center.df-direction-column
-      input.input.input__border.full-width(placeholder="Correo electrónico")
+      h1.color-white Inicia Sesión
+      .mb-2
+      input.input.input__border.full-width(
+        placeholder="Correo electrónico",
+        v-model="form.email"
+      )
+      .error-msg(v-if="$v.form.email.$model && $v.form.email.$invalid") Un correo válido es requerido
       .mb-2
       input.input.input__border.full-width(
         placeholder="Contraseña",
-        type="password"
+        type="password",
+        v-model.trim="form.password"
       )
+      .error-msg(v-if="$v.form.password.$model && !$v.form.password.required") Contraseña es requerido
+      .error-msg(v-if="$v.form.password.$model && !$v.form.password.minLength") La contraseña debe contener al menos 6 caracteres
       .mb-2
       .no-account.color-white.small-text
         | Aún no tienes cuenta? &nbsp;
         router-link.anchor(to="/sign-up") Haz click aquí
       .mb-2
-      button.button.button__gradient-background.font-bold.full-width Iniciar Sesión
+      button.button.button__gradient-background.font-bold.full-width(
+        :disabled="$v.form.$invalid"
+      ) Inicia Sesión
 </template>
 
 <script>
+import { validationMixin } from "vuelidate";
+import { required, email, minLength } from "vuelidate/lib/validators";
 export default {
+  mixins: [validationMixin],
   components: {
     Nav: () => import("@/components/Nav/Nav")
-  }
+  },
+  validations: {
+    form: {
+      email: {
+        required,
+        email
+      },
+      password: {
+        required,
+        minLength: minLength(6)
+      }
+    }
+  },
+  data: () => ({
+    form: {
+      email: "",
+      password: ""
+    }
+  })
 };
 </script>
 
--- src/views/Auth/SignUp.vue
+++ src/views/Auth/SignUp.vue
@@ -3,32 +3,81 @@
   Nav(onlyLogo, :spaceBetween="false")
   .sized-box.df.df-center
     .form-container.df.df-center.df-direction-column
-      input.input.input__border.full-width(placeholder="Nombre")
+      h1.color-white Regístrate
       .mb-2
-      input.input.input__border.full-width(placeholder="Correo electrónico")
+      input.input.input__border.full-width(
+        placeholder="Nombre",
+        v-model.trim="form.name"
+      )
+      .error-msg(v-if="$v.form.name.$model && $v.form.name.$invalid") Nombre es requerido
+      .mb-2
+      input.input.input__border.full-width(
+        placeholder="Correo electrónico",
+        v-model.trim="form.email"
+      )
+      .error-msg(v-if="$v.form.email.$model && $v.form.email.$invalid") Un correo válido es requerido
       .mb-2
       input.input.input__border.full-width(
         placeholder="Contraseña",
-        type="password"
+        type="password",
+        v-model.trim="form.password"
       )
+      .error-msg(v-if="$v.form.password.$model && !$v.form.password.required") Contraseña es requerido
+      .error-msg(v-if="$v.form.password.$model && !$v.form.password.minLength") La contraseña debe contener al menos 6 caracteres
       .mb-2
       input.input.input__border.full-width(
         placeholder="Confirmar contraseña",
-        type="password"
+        type="password",
+        v-model.trim="form.confirmPassword"
       )
+      .error-msg(
+        v-if="$v.form.confirmPassword.$model && $v.form.confirmPassword.$invalid"
+      ) Las contraseñas no coinciden
       .mb-2
       .no-account.color-white.small-text
         | Ya tienes una cuenta? &nbsp;
         router-link.anchor(to="/login") Haz click aquí
       .mb-2
-      button.button.button__gradient-background.font-bold.full-width Crear cuenta
+      button.button.button__gradient-background.font-bold.full-width(
+        :disabled="$v.form.$invalid"
+      ) Crear cuenta
 </template>
 
 <script>
+import { validationMixin } from "vuelidate";
+import { required, email, minLength, sameAs } from "vuelidate/lib/validators";
+
 export default {
+  mixins: [validationMixin],
   components: {
     Nav: () => import("@/components/Nav/Nav")
-  }
+  },
+  validations: {
+    form: {
+      name: {
+        required
+      },
+      email: {
+        required,
+        email
+      },
+      password: {
+        required,
+        minLength: minLength(6)
+      },
+      confirmPassword: {
+        sameAsPassword: sameAs("password")
+      }
+    }
+  },
+  data: () => ({
+    form: {
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: ""
+    }
+  })
 };
 </script>
 
