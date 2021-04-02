19d7ad8b566f43a28ace40d41c820509c934ea07
Daniel Apodaca <apodacaduron@gmail.com>
2021-04-02 15:44:48 -0700

    build

---
+++ docs/precache-manifest.ffc9d6de6737b3bed602f0c90551439f.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37ed9ee967952a934f6d",
+    "url": "/vue-tasks/css/app.0641e0db.css"
+  },
+  {
+    "revision": "a1bc6bfae67f016b7a76",
+    "url": "/vue-tasks/css/chunk-2dff7a10.8ad8068d.css"
+  },
+  {
+    "revision": "3cc86f11860c8a4cc4b4",
+    "url": "/vue-tasks/css/chunk-44277b54.c69d934d.css"
+  },
+  {
+    "revision": "ac758e9466193fdf7035",
+    "url": "/vue-tasks/css/chunk-72565651.d1cd6eed.css"
+  },
+  {
+    "revision": "093d9250a9af2fa3a171",
+    "url": "/vue-tasks/css/chunk-e56336fa.86f65bbe.css"
+  },
+  {
+    "revision": "3e8ea358124ce2b9eac3",
+    "url": "/vue-tasks/css/sign-up.20b85df0.css"
+  },
+  {
+    "revision": "a47f590fdf71c00a584f",
+    "url": "/vue-tasks/css/tasks.8a2ac1a9.css"
+  },
+  {
+    "revision": "de28a4e450ded72ad62d1ec40b4c5c64",
+    "url": "/vue-tasks/img/arrow-down.de28a4e4.svg"
+  },
+  {
+    "revision": "071effd3bc14e9865b0927875cb05a4e",
+    "url": "/vue-tasks/img/bg.071effd3.jpg"
+  },
+  {
+    "revision": "85a0abaadcc26604fbe13654ba3f1f49",
+    "url": "/vue-tasks/img/github.85a0abaa.svg"
+  },
+  {
+    "revision": "9bd8b06634cd00f4798e55a47516c702",
+    "url": "/vue-tasks/img/plus.9bd8b066.svg"
+  },
+  {
+    "revision": "6dc7eafd1a0547c1667792c670b5597c",
+    "url": "/vue-tasks/img/preview.png"
+  },
+  {
+    "revision": "b5c41cdcc04d77fa85bdca099d91f331",
+    "url": "/vue-tasks/img/x.b5c41cdc.svg"
+  },
+  {
+    "revision": "4ce6f52baecd63e0480f883ed1a8d49e",
+    "url": "/vue-tasks/index.html"
+  },
+  {
+    "revision": "37ed9ee967952a934f6d",
+    "url": "/vue-tasks/js/app.16b2c986.js"
+  },
+  {
+    "revision": "a1bc6bfae67f016b7a76",
+    "url": "/vue-tasks/js/chunk-2dff7a10.bb04ad8d.js"
+  },
+  {
+    "revision": "3cc86f11860c8a4cc4b4",
+    "url": "/vue-tasks/js/chunk-44277b54.ff2d6723.js"
+  },
+  {
+    "revision": "ac758e9466193fdf7035",
+    "url": "/vue-tasks/js/chunk-72565651.cea3481a.js"
+  },
+  {
+    "revision": "093d9250a9af2fa3a171",
+    "url": "/vue-tasks/js/chunk-e56336fa.0fb76f33.js"
+  },
+  {
+    "revision": "3afda1bcd9ecef529bbf",
+    "url": "/vue-tasks/js/chunk-vendors.f6faa2ce.js"
+  },
+  {
+    "revision": "3e8ea358124ce2b9eac3",
+    "url": "/vue-tasks/js/sign-up.d5a37159.js"
+  },
+  {
+    "revision": "1f5e7f160561437d37aa",
+    "url": "/vue-tasks/js/sign-up~tasks.a46be830.js"
+  },
+  {
+    "revision": "a47f590fdf71c00a584f",
+    "url": "/vue-tasks/js/tasks.6cb09703.js"
+  },
+  {
+    "revision": "8daa1f6f4f03123948453c23a4a67716",
+    "url": "/vue-tasks/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-tasks/robots.txt"
+  }
+]);
