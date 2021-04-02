9d4c1e5863e459eee9fc450d1c9434ccfbbb7892
Daniel Apodaca <apodacaduron@gmail.com>
2021-04-02 15:44:25 -0700

    fix(docs): build to docs

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,4 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === "production" ? "/vue-tasks/" : "/",
-  configureWebpack: {
-    output: {
-      path: __dirname + "/docs"
-    }
-  }
+  outputDir: __dirname + "/docs"
 };
